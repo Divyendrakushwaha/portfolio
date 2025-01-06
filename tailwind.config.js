42d98e58791366544544f7fd084fcb625c071985
Divyendra Kushwaha <divyendrak@contatan.local>
2025-01-06 23:31:10 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",       // Ensure Tailwind scans your index.html
+      "./src/**/*.{js,ts,jsx,tsx}", // Include all JavaScript and TypeScript files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
